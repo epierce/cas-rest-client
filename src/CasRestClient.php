b1e564cbc18078e623973671e18f6caacff6e278
Eric Pierce <epierce@usf.edu>
2014-12-18 16:05:01 -0500

    CAS login is working

--- src/CasRestClient.php
+++ src/CasRestClient.php
@@ -1,7 +1,8 @@
 <?php
-namespace epierce\cas;
+namespace epierce;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class CasRestClient {
 
@@ -13,36 +14,34 @@ class CasRestClient {
     private $cas_ticket_url = '/cas/v1/tickets';
     private $cas_username;
     private $cas_password;
+    
+    private $tgt_location;
 
     public function __construct()
     {
         $this->guzzle_client = new Client();
     }
 
-    public function setDebug(boolean $value = TRUE)
+    public function setDebug($value = TRUE)
     {
         $this->debug = $value;
     }
 
-    public function setCasServer(string $server){
+    public function setCasServer($server){
         $this->cas_server = $server;
     }
 
-    public function setCredentials(string $username, string $password){
+    public function setCredentials($username, $password){
         $this->cas_username = $username;
         $this->cas_password = $password;
     }
 
-    public function setTicketUrl(string $ticket_url){
+    public function setTicketUrl($ticket_url){
         $this->cas_ticket_url = $ticket_url;
     }
 
-    public function verifySSL(boolean $value, string $ca_bundle_path = ''){
+    public function verifySSL($value = TRUE){
         $this->verify_ssl = $value;
-
-        if (($ca_bundle_path) && (is_readable($ca_bundle_path))){
-            $this->ca_bundle_path = $ca_bundle_path;
-        }
     }
 
     public function login(){
@@ -51,15 +50,32 @@ class CasRestClient {
             throw new \Exception ('CAS server and credentials must be set before calling login()', 1);
         }
 
-        $request = $this->guzzle_client->createRequest('POST', $this->cas_server.$this->cas_ticket_url);
+        $request = $this->guzzle_client->createRequest( 'POST',
+                                                        $this->cas_server.$this->cas_ticket_url,
+                                                        [
+                                                            'cookies' => TRUE,
+                                                            'verify' => $this->verify_ssl,
+                                                            'body' => [
+                                                                'username' => $this->cas_username,
+                                                                'password' => $this->cas_password
+                                                            ]
+                                                        ]);
 
-        $postBody = $request->getBody();
-        $postBody->setField('username', $this->cas_username);
-        $postBody->setField('password', $this->cas_password);
-
-        $response = $this->guzzle_client->send($request);
-        $response_headers = $response->getHeaders();
-        $response_body = $response->getBody();
+        try {
+            $response = $this->guzzle_client->send ($request);
+            $response_headers = $response->getHeaders ();
+            $response_body = $response->getBody ();
+        } catch (ClientException $e) {
+            // Bad username or password.
+            if ($e->getCode() == 400){
+                if($this->debug) echo "Login Failed!  Bad username or password!\n";
+                return FALSE;
+            } else {
+                throw new \Exception ($e->getMessage(), $e->getCode());
+            }
+        } catch (\Exception $e) {
+            throw new \Exception ($e->getMessage(), $e->getCode());
+        }
 
         if($this->debug){
             echo "=======================\n";
@@ -68,12 +84,16 @@ class CasRestClient {
             echo "TGT Request: POST URL\n";
             echo $this->cas_server.$this->cas_ticket_url."\n";
             echo "TGT Request: POST body\n";
-            echo json_encode($postBody->getFields())."\n";
-            echo "TGT Response: Headers\n";
-            echo json_encode($response_headers)."\n";
-            echo "TGT Response: Body\n";
+            print_r($request->getBody()->getFields());
+            echo "\nTGT Response: Headers\n";
+            print_r($response_headers);
+            echo "\nTGT Response: Body\n";
             echo $response_body."\n";
             echo "\n=======================\n";
+        }
+
+        if (isset($response_headers['Location'])){
+            $this->tgt_location;
         }
 
 
